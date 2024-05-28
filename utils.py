3a63b26c05a41dd9fbe750f567c0280a57e4fd94
dam900 <damianjakubowski48@gmail.com>
2024-05-28 17:47:36 +0200

    added doc strings

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
+"""Utils"""
+
+
 def add(a: int, b: int) -> int:
+    """Add"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Add"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Add"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Add"""
     return a / b
